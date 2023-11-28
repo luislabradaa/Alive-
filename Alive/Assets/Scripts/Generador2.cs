d254449e933c125400ed2110383bfae452ccb88f
luislabradaa <swirly.olive@gmail.com>
2023-11-28 17:22:14 -0600

    Generar monedas en nivel 2 y daño de enemigos

--- Alive/Assets/Scripts/Generador2.cs
+++ Alive/Assets/Scripts/Generador2.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Generador : MonoBehaviour
+public class Generador2 : MonoBehaviour
 {
     public GameObject[] monedasPrefabs;
     private float yPosition = 0.1f;
@@ -10,14 +10,14 @@ public class Generador : MonoBehaviour
      private void Start()
     {
         GenerarMoneda();
-        // Llamar al método GenerarMoneda cada 15 segundos.
-        InvokeRepeating("GenerarMoneda", 0f, 15f);
+        // Llamar al método GenerarMoneda cada 10 segundos.
+        InvokeRepeating("GenerarMoneda", 0f, 0.1f);
     }
 
     private void GenerarMoneda()
     {
         int indexMoneda = Random.Range(0, monedasPrefabs.Length);
-        Vector3 spawnPosition = new Vector3(Random.Range(220, 260), yPosition, Random.Range(380, 313));
+        Vector3 spawnPosition = new Vector3(Random.Range(186, 340), yPosition, Random.Range(450, 280));
         Instantiate(monedasPrefabs[indexMoneda], spawnPosition, monedasPrefabs[indexMoneda].transform.rotation);
     }
     
